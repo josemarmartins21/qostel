3d72b0ebbc57033747fc7fd5f3f34d1b8051cf1f
josemarmartins21 <josemar21@outlook.pt>
2026-05-09 08:06:43 +0100

    Add arquivo de sobre a empresa

---
+++ inc/sobre.php
@@ -0,0 +1,12 @@
+<!-- Secção da empresa -->
+<section id="sobre">
+    <h2>Quem Somos?</h2>
+    
+    <article>
+        <p>
+            A QoS Tel é uma empresa de direito angolano,
+            Licenciada pelo INACOM, vocacionada na oferta de
+            soluções nos Serviços de Telecomunicações para o mercado empresarial e residencial. Situada na província de Luanda, Município do Kilamba Kiaxi, Distrito Urbano de 11 de Novembro, Edificio Prest Service 1º andar.
+        </p>
+    </article>
+</section>
