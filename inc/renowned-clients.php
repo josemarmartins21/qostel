8015a5fe87b568d41f2cae651c3d12d06687b5fb
josemarmartins21 <josemar21@outlook.pt>
2026-05-10 07:38:38 +0100

    Atualizei o caminho para imagens de logo dos clientes

--- inc/renowned-clients.php
+++ inc/renowned-clients.php
@@ -2,10 +2,11 @@
     <div class="fundo-preto-2">
         <h2>Nossos Clientes</h2>
         <div id="logos-container">
+            <?php dd($renownedClients->getResources()); ?>
             <?php foreach($renownedClients->getResources() as $client): ?>
                 <a href="<?= htmlspecialchars($client->url) ?>" title="<?= htmlspecialchars($client->name) ?>" target="_blank" >
                     <img
-                        src="/assets/images/logos_client/<?= htmlspecialchars($client->logo) ?>"
+                        src="../assets/images/logos_client/<?= htmlspecialchars($client->logo) ?>"
                         alt="<?= htmlspecialchars($client->name) ?>"
                         class="logo-empresa"
                     >
